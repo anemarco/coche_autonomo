f57c368140334e164e4d7dc6479f152d8cafb4f1
iness <iness@DESKTOP-T3BES9L>
2021-11-09 08:36:57 +0100

    Herencia

---
+++ src/Simulador/ObjetoSimulacion.java
@@ -0,0 +1,61 @@
+package Simulador;
+
+public abstract class ObjetoSimulacion {
+	
+	protected double x, y;
+	protected double a, h;	
+	
+	/**Constructor
+	 * @param x Coordenada x del centro visual del objeto
+	 * @param y Coordenada y del centro visual del objeto
+	 * @param a Anchura del objeto
+	 * @param h Altura del objeto
+	 */
+	
+	public ObjetoSimulacion(double x, double y, double a, double h) {
+		this.x = x;
+		this.y = y;
+		this.a = a;
+		this.h = h;
+	}
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getA() {
+		return a;
+	}
+
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+
+	public double getH() {
+		return h;
+	}
+
+	public void setH(double h) {
+		this.h = h;
+	}
+
+	/**Método que permite dibujar el objeto en la ventana gráfica
+	 */
+
+	public abstract void dibujar();
+
+}
