c09b064a80a1f24ce353f881d88c3f36aaeacd44
iness <iness@DESKTOP-T3BES9L>
2021-11-10 12:21:05 +0100

    Prueba

--- src/Ventanas/VentanaSimulador.java
+++ src/Ventanas/VentanaSimulador.java
@@ -30,8 +30,8 @@ public class VentanaSimulador extends JFrame {
 		});
 	}
 
-	/**Constructor de ventana
-	 */
+	/**Constructor de ventana*/
+	
 	public VentanaSimulador() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
