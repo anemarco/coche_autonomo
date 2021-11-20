566ef2ac8236c2788124c94c3dc415c731398f38
Ane <ane.marco@opendeusto.es>
2021-11-20 16:39:49 +0100

    Hacer que el coche vaya hacia delante en una velocidad constante.

--- src/ventanas/VentanaSimulador.java
+++ src/ventanas/VentanaSimulador.java
@@ -61,59 +61,8 @@ public class VentanaSimulador extends JFrame {
 		//simuladorPane.setSize(TAM_VENT.width-20,TAM_VENT.height-10);
 		
 		//escalar im�genes
-		Image fondoImg = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (1).jpg")).getImage();
+		Image fondoImg = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (16).jpg")).getImage();
 		ImageIcon fondo = new ImageIcon(fondoImg.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg2 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (2).jpg")).getImage();
-		ImageIcon fondo2 = new ImageIcon(fondoImg2.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg3 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (3).jpg")).getImage();
-		ImageIcon fondo3 = new ImageIcon(fondoImg3.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg4 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (4).jpg")).getImage();
-		ImageIcon fondo4 = new ImageIcon(fondoImg4.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg5 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (5).jpg")).getImage();
-		ImageIcon fondo5 = new ImageIcon(fondoImg5.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg6 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (6).jpg")).getImage();
-		ImageIcon fondo6 = new ImageIcon(fondoImg6.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg7 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (7).jpg")).getImage();
-		ImageIcon fondo7 = new ImageIcon(fondoImg7.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg8 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (8).jpg")).getImage();
-		ImageIcon fondo8 = new ImageIcon(fondoImg8.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg9 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (9).jpg")).getImage();
-		ImageIcon fondo9 = new ImageIcon(fondoImg9.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg10 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (10).jpg")).getImage();
-		ImageIcon fondo10 = new ImageIcon(fondoImg10.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg11 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (11).jpg")).getImage();
-		ImageIcon fondo11 = new ImageIcon(fondoImg11.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg12 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (12).jpg")).getImage();
-		ImageIcon fondo12 = new ImageIcon(fondoImg12.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg13 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (13).jpg")).getImage();
-		ImageIcon fondo13 = new ImageIcon(fondoImg13.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg14 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (14).jpg")).getImage();
-		ImageIcon fondo14 = new ImageIcon(fondoImg14.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg15 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (15).jpg")).getImage();
-		ImageIcon fondo15 = new ImageIcon(fondoImg15.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg16 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (16).jpg")).getImage();
-		ImageIcon fondo16 = new ImageIcon(fondoImg16.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg17 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (17).jpg")).getImage();
-		ImageIcon fondo17 = new ImageIcon(fondoImg17.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
-		
-		Image fondoImg18 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE (18).jpg")).getImage();
-		ImageIcon fondo18 = new ImageIcon(fondoImg18.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
 		
 		Image cocheImg = new ImageIcon(getClass().getResource("../simulador/img/coche.png")).getImage();
 		ImageIcon coche = new ImageIcon(cocheImg.getScaledInstance(100, 150, Image.SCALE_SMOOTH));
@@ -123,9 +72,7 @@ public class VentanaSimulador extends JFrame {
 		label2.setBounds(280, 200, 515, 513);
 		simuladorPane.add(label2);
 		//JPanel.add(image, BorderLayout.NORTH);
-		JLabel label = new JLabel(fondo);
-		label.setBounds(10, 10, 966, 542);
-		simuladorPane.add(label);
+		
 		
 		//Crear panel para botones 
 		JPanel panelBotonero= new JPanel();
@@ -149,6 +96,48 @@ public class VentanaSimulador extends JFrame {
 		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,panelBotonero,simuladorPane);
 		
 		cp.add(splitPane);
+		
+		Thread time = new Thread(){
+            public void run(){
+            	simuladorPane.setLayout(null);
+        		//JPanel.add(image, BorderLayout.NORTH);
+        		JLabel label = new JLabel(fondo);
+        		label.setBounds(10, 10, 966, 542);
+        		simuladorPane.add(label);
+        		int num=15;
+        		 while(true){
+             		try {
+							sleep(400);
+						} catch (InterruptedException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+                	Image fondoImg2 = new ImageIcon(getClass().getResource("../simulador/img/FONDO COCHE ("+ num +").jpg")).getImage();
+            		ImageIcon fondo2 = new ImageIcon(fondoImg2.getScaledInstance(TAM_VENT.width,TAM_VENT.height, Image.SCALE_SMOOTH));
+            		simuladorPane.setLayout(null);
+            		simuladorPane.remove(label);
+            		label.setVisible(false);
+            		
+            		JLabel picSecond = new JLabel(fondo2);
+            		picSecond.setBounds(10, 10, 966, 542);
+            		simuladorPane.add(picSecond);
+            		picSecond.setVisible(true);
+            		simuladorPane.revalidate();
+            		simuladorPane.repaint();
+            		
+            		label = picSecond;
+            		            		
+            		num = num-1;
+            		if (num==0){
+            			num = 16;  		
+                    }
+            		
+
+
+                }
+            }
+        };
+        time.start();
 		
 	}
 	
