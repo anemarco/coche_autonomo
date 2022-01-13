e0ed4c79278db0f26dbf15e78214c401b0217c7e
Ane <ane.marco@opendeusto.es>
2022-01-13 13:34:15 +0100

    Junit de simulador

--- test/test/TestSimulador.java
+++ test/test/TestSimulador.java
@@ -10,13 +10,15 @@ import org.junit.jupiter.api.Test;
 
 import simulador.Animal;
 import simulador.OtroCoche;
+import simulador.Semaforo;
 import simulador.Coche;
 import simulador.Obstaculo;
 
 class TestSimulador {
 	
-	Coche c1= new Coche(0, 0, 0, 0, 0, 0);
-	
+	Coche c1= new Coche(0, 0);
+	Obstaculo obs= new OtroCoche(10,15); 
+
 	@Before
 	public void setUp() throws Exception {
 		
@@ -32,17 +34,13 @@ class TestSimulador {
 		c1.acelerar(4);
 		assertEquals(4, c1.getVelocidad());
 	}
-	//Comprobar el metodo girar de la clase Coche
-	@Test
-	void testGirar() {
-	
-	}
+
 	//Comprobar el metodo choca de la clase Coche
 	@Test
 	void testChoca() {
 		OtroCoche obst1= new OtroCoche(0,0);
 		Animal obst2= new Animal();
-		assertTrue( c1.choca(obst1) );
+		assertTrue( c1.choca(obst1));
 		assertFalse(c1.choca(obst2));
 	}
 	//Comprobar el metodo acelerar de la clase Coche y Obstaculo
@@ -53,22 +51,13 @@ class TestSimulador {
 		assertEquals(5, c1.getX());
 		assertEquals(3, c1.getY());
 		//para obst�culo
-		Obstaculo obs= new  OtroCoche(10,15);
-		obs.mover(4, 0);
-		assertEquals(14, obs.getX());
-		assertEquals(15, obs.getY());
-	}
-	@Test
-	void testMovimientoCarretera() {
 		
+		obs.mover(4, 1); 
+		assertEquals(14, obs.getX()); 
+		assertEquals(16, obs.getY());
+		 
 	}
-	
-	//@Test
-	//public Obstaculo testDetectaObs(Obstaculo obs) {
-		//double dist = Math.sqrt(Math.pow(s1.getX()-obs.getX(), 2)+Math.pow(s1.getY()-obs.getY(), 2));
-		
-	//}
-	
 	
 
+	
 }
